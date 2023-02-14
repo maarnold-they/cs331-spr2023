6e85040116bb25880f3651bfcf66a52d1e93b317
imthegngrbrdmn <clear.the.spam@gmail.com>
2023-02-13 17:12:04 -0900

    Pass Test Suite collatz

--- hw02/pa2.lua
+++ hw02/pa2.lua
@@ -9,6 +9,7 @@
 local pa2 = {}
 
 function pa2.mapTable(inputFunction, inputTable)
+  local key, value
   for key, value in pairs(inputTable) do
     inputTable[key] = inputFunction(value) 
   end
@@ -16,7 +17,7 @@ function pa2.mapTable(inputFunction, inputTable)
 end
 
 function pa2.concatMax(inputString, maxLength)
-  modifiedString = ""
+  local modifiedString = ""
   while maxLength >= (string.len(modifiedString) + string.len(inputString)) do
     modifiedString = modifiedString .. inputString
   end
@@ -24,7 +25,18 @@ function pa2.concatMax(inputString, maxLength)
 end
 
 function pa2.collatz(k)
-  return k
+  return function ()
+    if k == 0 then return nil end
+   
+    local lastK = k
+    
+    if k == 1 then k = 0 end
+   
+    if k % 2 == 1 then k = 3 * k + 1 
+    else k = k / 2 end
+    
+    return lastK
+  end
 end
 
 function pa2.backSubs(s)
