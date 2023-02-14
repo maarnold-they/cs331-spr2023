c2b726e2ce0ce72b638050853e92d75531d6a57b
imthegngrbrdmn <clear.the.spam@gmail.com>
2023-02-13 16:33:21 -0900

    init
    Create initial file for Assignment 2 functions

---
+++ hw02/pa2.lua
@@ -0,0 +1,27 @@
+-- pa2.lua
+-- Millard A. Arnold V
+-- 2023-02-13
+-- 
+-- For CS 331 Spring 2023
+-- Assignment 2 Functions
+-- Used in Assignment 2, Exercise B
+
+local pa2 = {}
+
+function pa2.mapTable(f, t)
+  return t
+end
+
+function pa2.concatMax(s, i)
+  return s
+end
+
+function pa2.collatz(k)
+  return k
+end
+
+function pa2.backSubs(s)
+  return s
+end
+
+return pa2
