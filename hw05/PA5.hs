46822b776cc6710ab74e11dead60db9ca68cfad3
imthegngrbrdmn <clear.the.spam@gmail.com>
2023-03-30 12:52:35 -0800

    Update PA5.hs
    All tests passed

--- hw05/PA5.hs
+++ hw05/PA5.hs
@@ -1,4 +1,8 @@
--- PA5.hs  SKELETON
+-- PA5.hs
+-- Millard A. Arnold 
+-- 2023-03-30
+-- 
+-- Based on skeleton by:
 -- Glenn G. Chappell
 -- 2023-03-22
 --
@@ -47,7 +51,12 @@ list1 ## list2 = length matches where
 
 -- filterAB
 filterAB :: (a -> Bool) -> [a] -> [b] -> [b]
-filterAB _ _ bs = bs  -- DUMMY; REWRITE THIS!!!
+filterAB _ _ [] = []
+filterAB _ [] _ = []
+filterAB op (a:as) (b:bs)
+  | op a = b:rest
+  | otherwise = rest where
+    rest = filterAB op as bs
 
 
 -- =====================================================================
@@ -55,15 +64,9 @@ filterAB _ _ bs = bs  -- DUMMY; REWRITE THIS!!!
 
 -- concatEvenOdd
 concatEvenOdd :: [String] -> (String, String)
-{-
-  The assignment requires concatEvenOdd to be written as a fold.
-  Like this:
-
-    concatEvenOdd xs = fold* ... xs  where
-        ...
-
-  Above, "..." should be replaced by other code. "fold*" must be one of
-  the following: foldl, foldr, foldl1, foldr1.
--}
-concatEvenOdd _ = ("Yo", "Yoyo")  -- DUMMY; REWRITE THIS!!!
+concatEvenOdd list1 = (foldl (++) [] (even list1), foldl (++) [] (odd list1)) where
+  odd [] = [] 
+  odd (_:xs) = even xs
+  even [] = []
+  even (x:xs) = x:odd xs
 
